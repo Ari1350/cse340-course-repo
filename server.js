61859316f867cad49afeb4492ed7fb9bfa9464ef
Guadalupe <ariasperaltag@gmail.com>
2026-05-15 23:11:38 -0400

    Feat: Connect database and fix organizations view

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 const PORT = process.env.PORT || 3000;
@@ -23,8 +25,10 @@ app.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
 });
 
-app.get('/organizations', (req, res) => {
-    res.render('organizations', { title: 'Organizations' });
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();    
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', (req, res) => {
@@ -35,8 +39,14 @@ app.get('/categories', (req, res) => {
     res.render('categories', { title: 'Service Project Categories' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async() => {
+ try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`);
+    } catch (error) {
+    console.error('Failed to connect to the database:', error);
+    process.exit(1); 
+  }
 });
 
